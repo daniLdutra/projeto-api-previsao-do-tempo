097e24a6f83679123afaa05f0b3b8ffc8a6a1211
Daniella Dutra <danivetdutra@gmail.com>
2021-12-04 12:31:19 -0300

    Dados-api inseridos para visualizaçao

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   //Pegando localização do usuário com hook do state location;
   const [location, setLocation] = useState(false);
   //Criado novo state para guardar os dados que veem da API
-  const [weather, setWeather] = useState(false);
+  const [weather, setWeather] = useState({});
 
   //Incluido 'function expression': responsável por chamar a API
   let getWeather = async (lat, long) => {
@@ -13,15 +13,16 @@ function App() {
       'http://api.openweathermap.org/data/2.5/weather',
       {
         params: {
-          lat: lat,
+          lat,
           lon: long,
-          appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+          appid: process.env.REACT_APP_OPEN_WEATHER_KEY,
           lang: 'pt',
           units: 'metric',
         },
       }
     );
     setWeather(res.data);
+    console.log(res.data);
   };
 
   // Será executado assim que o app for montado e sempre que muda algo, ele é chamado novamente.
@@ -39,14 +40,21 @@ function App() {
   return (
     (location && (
       <>
-        <h3>Clima na sua região (Exemplo)</h3>
+        <h3>
+          Clima na sua região (
+          {weather &&
+            weather.weather &&
+            weather.weather[0] &&
+            weather.weather[0].description}
+          )
+        </h3>
         <hr />
         <ul>
-          <li>Temperatura atual: </li>
-          <li>Temperatura máxima: </li>
-          <li>Temperatura mínima: </li>
-          <li>Pressão: hpa</li>
-          <li>Umidade: %</li>
+          <li>Temperatura atual: {weather?.main?.temp} </li>
+          <li>Temperatura máxima: {weather?.main?.temp_max} </li>
+          <li>Temperatura mínima: {weather?.main?.temp_min} </li>
+          <li>Pressão: {weather?.main?.pressure} hpa</li>
+          <li>Umidade: {weather?.main?.humidity} %</li>
         </ul>
       </>
     )) || <>Você precisa autorizar sua localização no Browser</>
