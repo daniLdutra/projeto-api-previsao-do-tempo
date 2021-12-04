365e7b1c3c51250e7d39667659d1edc2fe0b3f24
Daniella Dutra <danivetdutra@gmail.com>
2021-12-04 10:24:55 -0300

    Realizada logica para autorizaçao da localizaçao do user

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  //Pegando localização do usuário com hook do state location;
+  const [location, setLocation] = useState(false);
+
+  // Será executado assim que o app for montado e sempre que muda algo, ele é chamado novamente.
+
+  useEffect(() => {
+    //código que pega as coordenadas do usuário, pedindo para o Browser
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.longitude, position.coords.latitude);
+      setLocation(true);
+    });
+  }, []);
+
+  //Verificação se o usuário autorizou compartilhar a localização
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    (location && (
+      <>
+        <h3>Clima na sua região (Exemplo)</h3>
+        <hr />
+        <ul>
+          <li>Temperatura atual: </li>
+          <li>Temperatura máxima: </li>
+          <li>Temperatura mínima: </li>
+          <li>Pressão: hpa</li>
+          <li>Umidade: %</li>
+        </ul>
+      </>
+    )) || <>Você precisa autorizar sua localização no Browser</>
   );
+  // if (location === false) {
+  //   return <>Você precisa autorizar sua localização no Browser</>;
+  // } else {
+  //   return (
+  //     <>
+  //       <h3>Clima na sua região (Exemplo)</h3>
+  //       <hr />
+  //       <ul>
+  //         <li>Temperatura atual: </li>
+  //         <li>Temperatura máxima: </li>
+  //         <li>Temperatura mínima: </li>
+  //         <li>Pressão: hpa</li>
+  //         <li>Umidade: %</li>
+  //       </ul>
+  //     </>
+  //   );
+  // }
 }
 
 export default App;
